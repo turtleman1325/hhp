7059a9d956f6aaf370b63be63484014d7f1e5245
Art <art19ad4@gmail.com>
2015-07-09 15:30:15 -0700

    Added navbar and static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,25 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about_us
+  end
+
+  def donate
+  end
+
+  def projects
+  end
+
+  def contact_us
+  end
+
+  def collections
+  end
+
+  def annual_report
+  end
+
+  def book_store
+  end
+end
